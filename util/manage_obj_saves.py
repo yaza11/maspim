283f56ae97a2a9f986a0dadd916644e69432682f
yaza11 <116643078+yaza11@users.noreply.github.com>
2024-05-02 09:30:53 +0200

    Add files via upload

--- util/manage_obj_saves.py
+++ util/manage_obj_saves.py
@@ -1,18 +1,31 @@
+"""
+This module defines which attributes are saved for each object when the save method is called.
+
+Using this strategy ensures that 'clean' instances of objects are saved but will remove custom user attributes.
+Also, this removes redundancy for the Image related classes: it is useful to set the original image as an attribute
+but not to save it on disc because the image file itself is already stored on disc somewhere. Hence, only saving the
+path to the image file on disc is sufficient and equally fast to load.
+
+"""
+
 Image_disc_attributes = {
     'path_folder',
     'age_span'
-    }
+}
 
 ImageSample_disc_attributes = Image_disc_attributes | {
-    'obj_color', 'xywh_ROI'}
+    'obj_color', '_xywh_ROI', '_hw', 'path_folder', 'path_image_file'}
 
 ImageROI_disc_attributes = ImageSample_disc_attributes | {
-    'image_classification', 'params_classification', 'punchholes'
+    '_image_classification', '_params', '_punchholes', '_punchhole_size', '_image_original'
 }
 
 ImageClassified_disc_attributes = ImageSample_disc_attributes | {
     'params_laminae_simplified',
-    'image_seeds'}
+    'image_seeds',
+    '_image_classification',
+    '_image_original'
+}
 
 ImageTransformation_disc_attributes = {'_section, _window_from', '_window_to'}
 
@@ -63,20 +76,21 @@ MetaFeatures_disc_attributes = {
 SampleImageHandlerMSI_attributes = {'extent_spots', 'path_folder', 'path_d_folder'}
 
 SampleImageHandlerXRF_attributes = {
-    'path_folder', 
-    'path_image_file', 
+    'path_folder',
+    'path_image_file',
     'path_image_roi_file',
     'ROI_is_image',
     'extent_spots',
-    'extent'
+    'extent',
+    'scale_conversion'
 }
 
 Spectra_attributes = {
-    'path_d_folder', 
-    'delta_mz', 'mzs', 
-    'intensities', 
-    'indices', 
-    'limits', 
+    'path_d_folder',
+    'delta_mz', 'mzs',
+    'intensities',
+    'indices',
+    'limits',
     'peaks',
     'peak_properties',
     'peak_setting_parameters',
@@ -119,4 +133,8 @@ name_to_attrs = {
 
 def class_to_attributes(class_object: object) -> set[str]:
     c: str = class_object.__class__.__name__
-    return name_to_attrs[c]
+    attrs = name_to_attrs.get(c)
+    if attrs is None:
+        print(f'found no entry for {c}, keeping all attributes')
+        attrs = class_object.__dict__.keys()
+    return attrs
