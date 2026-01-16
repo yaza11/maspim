8ada8d9cbf2c7bd279bcefdd6917f9e9f4f235b5
yaza11 <116643078+yaza11@users.noreply.github.com>
2026-01-16 11:40:32 +0100

    fix requirements

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '1.4.9'
+VERSION = '1.4.10'
 DESCRIPTION = 'Processing of mass spectrometry imaging and micro X-Ray fluorescence data.'
 NAME = "maspim"
 
@@ -35,7 +35,7 @@ setup(
                       'psutil',
                       'h5py',
                       'pillow',
-                      'tables'
+                      'tables',
                       'textdistance',
                       'astropy'],
     extras_require={'dev': 'twine', 'all': ['mfe']},
